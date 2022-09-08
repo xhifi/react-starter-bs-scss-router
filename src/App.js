5df6c3f50cfa7363d18915659d716c8d7a9672ed
xhifi <shifa.newversion@gmail.com>
2022-09-09 03:25:13 +0500

    boilerplate created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h2>This is React!</h2>
+    </div>
+  );
+}
+
+export default App;
